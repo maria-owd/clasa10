bd2d9c5d9d8facd1e51ed963e454210f7206474f
Maria <maria.maiorescu@gmail.com>
2023-03-26 15:22:38 +0300

    tot matrici

--- 2023-02-28/ex9-v2.cpp
+++ 2023-02-28/ex9-v2.cpp
@@ -3,9 +3,9 @@
 #include <fstream>
 
 /**
- *  Se citeste o matrice cu n linii si m coloane, (n,m<=100) cu elemente numere naturale din intervalul
- *  [0,1000]. 
- *   Afisati liniile cu numar maxim de de elemente disticte.
+ *  Se citeste o matrice cu n linii si m coloane, (n,m<=100) 
+ *  cu elemente numere naturale din intervalul [0,1000]. 
+ *  Afisati liniile cu numar maxim de de elemente disticte.
  */
 
 using namespace std;
@@ -13,7 +13,8 @@ int main() {
 
     int a[100][100];
     int m = 5, n = 6;
-    int v[100], nr, maxim;
+    int valoareMax = 1001;
+    int c[valoareMax], v[100], nr, maxim;
     bool distinct;
 
     ifstream f("date9.in");
@@ -37,24 +38,25 @@ int main() {
 
     maxim = 0;
     for (int i = 0; i < m; i++) {
-        nr = 0;
+        for (int k = 0; k < valoareMax; k++) {
+            c[k] = 0;
+        }
+
         for (int j = 0; j < n; j++) {
-            distinct = true;
-            for (int k = 0; k < n; k++) {
-                if (k != j && a[i][j] == a[i][k]) {
-                    distinct = false;
-                }
-            }
-            if (distinct) {
+            c[a[i][j]]++; 
+        }
+
+        nr = 0;
+        for (int k = 0; k < valoareMax; k++) {
+            if (c[k] == 1) {
                 nr++;
             }
         }
         v[i] = nr;
-
-        if (v[i] > maxim) {
-            maxim = v[i];
-        }
         
+        if (nr > maxim) {
+            maxim = nr;
+        }      
     }
 
     for (int i = 0; i < m; i++) {
