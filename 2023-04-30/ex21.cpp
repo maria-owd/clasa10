61ac3b44961f2aeb467860bd5139b6321de49ad3
Maria <maria.maiorescu@gmail.com>
2023-04-30 15:21:27 +0300

    modificat

--- 2023-04-30/ex21.cpp
+++ 2023-04-30/ex21.cpp
@@ -10,6 +10,7 @@ int main () {
 
     int a[100][100];
     int n, produs;
+    bool par;
 
     ifstream f("date21.in");
     f >> n;
@@ -31,16 +32,23 @@ int main () {
     cout << endl;
 
     produs = 1;
+    par = false;
     for (int i = 0; i < n; i ++) {
         for (int j = 0; j < n; j++) {
             if (i==j) {
                 if (a[i][j] % 2 == 0) {
                     produs = produs * a[i][j];
+                    par = true;
                 }
             }
         }
     }
-    cout << "produsul nr pare de pe dp: " << produs << endl;
+
+    if (par) {
+        cout << "produsul nr pare de pe dp: " << produs << endl;
+    } else {
+        cout << "nu-s nr pare" << endl;
+    }
 
     return 0;
 }
